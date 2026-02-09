d5b38cf565c090d7cd0a31a19466e87f41e3120b
t-ESC <theo.krinitz@proton.me>
2026-02-09 15:22:40 +0100

    Flipped row/col in MS substitution

--- src/dialga/ms.rs
+++ src/dialga/ms.rs
@@ -4,29 +4,29 @@ pub const PI_M: [u8; 16] = [0, 0xa, 5, 0xf, 0xe, 4, 0xb, 1, 9, 3, 0xc, 6, 7, 0xd
 pub const PI_M_INV: [u8; 16] = [0, 7, 0xe, 9, 5, 2, 0xb, 0xc, 0xf, 8, 1, 6, 0xa, 0xd, 4, 3];
 
 pub fn ms(state: &mut State) {
-    for col in 0..4 {
-        for row in 0..4 {
-            let mut high_bits = state.0[col][row] >> 4;
-            let mut low_bits = state.0[col][row] & 0b00001111;
-            
+    for row in 0..4 {
+        for col in 0..4 {
+            let mut high_bits = state.0[row][col] >> 4;
+            let mut low_bits = state.0[row][col] & 0b00001111;
+
             high_bits = PI_M[high_bits as usize];
             low_bits = PI_M[low_bits as usize];
 
-            state.0[col][row] = (high_bits << 4) + low_bits;
+            state.0[row][col] = (high_bits << 4) + low_bits; 
         }
     }
 }
 
 pub fn ms_inv(state: &mut State) {
-    for col in 0..4 {
-        for row in 0..4 {
-            let mut high_bits = state.0[col][row] >> 4;
-            let mut low_bits = state.0[col][row] & 0b00001111;
+    for row in 0..4 {
+        for col in 0..4 {
+            let mut high_bits = state.0[row][col] >> 4;
+            let mut low_bits = state.0[row][col] & 0b00001111;
             
             high_bits = PI_M_INV[high_bits as usize];
             low_bits = PI_M_INV[low_bits as usize];
 
-            state.0[col][row] = (high_bits << 4) + low_bits;        
+            state.0[row][col] = (high_bits << 4) + low_bits;        
         }
     }
 }
