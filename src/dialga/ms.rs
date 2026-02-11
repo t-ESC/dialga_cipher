0a3367a64ec0e753a7f882cb03c40d8eaa6d21a7
t-ESC <theo.krinitz@proton.me>
2026-02-11 12:39:36 +0100

    Fixex bytepermutation (was substitution)

--- src/dialga/ms.rs
+++ src/dialga/ms.rs
@@ -4,29 +4,44 @@ pub const PI_M: [u8; 16] = [0, 0xa, 5, 0xf, 0xe, 4, 0xb, 1, 9, 3, 0xc, 6, 7, 0xd
 pub const PI_M_INV: [u8; 16] = [0, 7, 0xe, 9, 5, 2, 0xb, 0xc, 0xf, 8, 1, 6, 0xa, 0xd, 4, 3];
 
 pub fn ms(state: &mut State) {
-    for col in 0..4 {
-        for row in 0..4 {
-            let mut high_bits = state.0[col][row] >> 4;
-            let mut low_bits = state.0[col][row] & 0b00001111;
-            
-            high_bits = PI_M[high_bits as usize];
-            low_bits = PI_M[low_bits as usize];
+    let pre_perm = *state;
+    for row in 0..4 {
+        for col in 0..4 {
+            let idx = (col << 2) + row;
+            let pi_idx = PI_M[idx];
 
-            state.0[col][row] = (high_bits << 4) + low_bits;
+            let pi_col = pi_idx >> 2;
+            let pi_row = pi_idx & 0b0011;
+
+            state.0[pi_row as usize][pi_col as usize] = pre_perm.0[row][col];
         }
     }
 }
 
 pub fn ms_inv(state: &mut State) {
-    for col in 0..4 {
-        for row in 0..4 {
-            let mut high_bits = state.0[col][row] >> 4;
-            let mut low_bits = state.0[col][row] & 0b00001111;
-            
-            high_bits = PI_M_INV[high_bits as usize];
-            low_bits = PI_M_INV[low_bits as usize];
+    let pre_perm = *state;
+    for row in 0..4 {
+        for col in 0..4 {
+            let idx = (col << 2) + row;
+            let pi_idx = PI_M_INV[idx];
 
-            state.0[col][row] = (high_bits << 4) + low_bits;        
+            let pi_col = pi_idx >> 2;
+            let pi_row = pi_idx & 0b0011;
+
+            state.0[pi_row as usize][pi_col as usize] = pre_perm.0[row][col];
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::dialga::{helper::state::State, ms::{ms, ms_inv}};
+
+    #[test]
+    fn test_ms() {
+        let mut test_state = State::from(0x92a33e3c3115979441131a892119bed7);
+        ms(&mut test_state);
+        ms_inv(&mut test_state);
+        assert_eq!(State::from(0x92a33e3c3115979441131a892119bed7), test_state);
     }
 }
